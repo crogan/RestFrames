6a44709889cf278e8fa845154250771270afedce
Christopher Rogan <crogan@Christophers-iMac.local>
2016-08-08 22:21:50 -0400

    LinkDef

--- inc/RestFrames/RestFrames_LinkDef.h
+++ inc/RestFrames/RestFrames_LinkDef.h
@@ -1,5 +1,10 @@
 #ifdef __ROOTCLING__
-#include "RestFrames/RestFrames.hh"
+
+#pragma link off all typedefs;
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
 #pragma link C++ namespace RestFrames;
 using namespace RestFrames;
 
